5daf60b0c902f897bc02afbbbd0c1bfa43375865
annasylvester <annaleesylvester@gmail.com>
2019-02-01 16:31:20 -0500

    Default Song Lookup working

--- liri.js
+++ liri.js
@@ -32,22 +32,27 @@ function searchStuff(userRequest, userRequestDetail) {
     switch (userRequest) {
 
         // Calls the Bands in Town function
-        // case "concert-this":
-        //     break;
+        case "concert-this":
+            break;
 
             // Calls the Spotify function
         case "spotify-this-song":
             let songTitle = userRequestDetail;
-            getSpotifyInfo(songTitle);
+            // If no song is provided, default is The Sign by Ace of Base
+            if (songTitle === "") {
+                searchDefaultSong();
+            } else {
+                getSpotifyInfo(songTitle);
+            }
             break;
 
             // Calls the Axios OMDB function
-        // case "movie-this":
-        //     break;
+        case "movie-this":
+            break;
 
             // Calls the Do what it says function - which runs spotify function from the random.txt document
-        // case "do-what-it-says":
-        //     break;
+        case "do-what-it-says":
+            break;
     }
 }
 
@@ -78,5 +83,30 @@ function getSpotifyInfo(songTitle) {
     });
 }
 
+let defaultSong = "The Sign";
 
+// Search Default song function that looks up Ace of Base
+function searchDefaultSong() {
+    spotify.search({
+        type: 'track',
+        query: "The Sign Ace of Base"
+    }, function (err, data) {
+        if (err) {
+            return log('Error occurred getting Spotify data: ' + err);
+        }
 
+        let artistsArray = data.tracks.items[0].album.artists;
+        let artistsNames = [];
+
+        for (var i = 0; i < artistsArray.length; i++) {
+            artistsNames.push(artistsArray[i].name);
+        }
+        let artists = artistsNames.join(", ");
+
+        // Logs Spotify Data
+        log("Artist(s): " + artists);
+        log("Song: " + data.tracks.items[0].name);
+        log("Preview Link : " + data.tracks.items[0].preview_url)
+        log("Album: " + data.tracks.items[0].album.name);
+    });
+}
